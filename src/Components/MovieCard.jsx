99f64400620064e6c1bafd6c37db831325b77de1
Mohd Azimuddin <sfsuper2020@gmail.com>
2025-03-20 00:39:01 +0530

    added new style if rating>6 then it will display in yellow color

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -8,8 +8,8 @@ const MovieCard = ({ movie }) => {
         /> 
         <div className="p-4">
           <h2 className="text-xl font-semibold">{movie.title}</h2>
-          <p className="text-gray-400">Rating: {movie.vote_average}</p>
-          <p className="text-gray-400">Release Date: {movie.release_date}</p>
+          <p className="text-gray-300">Rating:<span className={`p-1 ${movie.vote_average>=6?"bg-yellow-200":"bg-red-700"} rounded-lg ml-1 mt-2 text-zinc-900 font-bold text-xl`}>{movie.vote_average}</span></p>
+          <p className="mt-2 text-gray-300">Release Date: <span className="text-white">{movie.release_date}</span></p>
         </div>
       </div>
     );
